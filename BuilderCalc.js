c8d79b5e5f965de2fef944f3b146e3c7e8f49e79
Migle Urbonaite <mig.urbonaite@gmail.com>
2022-10-05 12:10:43 +0200

    Builder caclulator

--- BuilderCalc.js
+++ BuilderCalc.js
@@ -1,1 +1,45 @@
 
+function validation(num) {
+	if (typeof num === "string" || typeof num === "undefined") {
+		throw new Error("Only numbers are allowed.");
+	}
+}
+
+class Calculator {
+	constructor(props) {
+		this.result = 0;
+	}
+
+	add(num) {
+		validation(num);
+		this.result = this.result + num;
+		return this;
+	}
+
+	subtract(num) {
+		validation(num);
+		this.result = this.result - num;
+		return this;
+	}
+
+	divide(num) {
+		validation(num);
+		this.result = this.result / num;
+		return this;
+	}
+
+	multiply(num) {
+		validation(num);
+		this.result = this.result * num;
+		return this;
+	}
+
+	compute() {
+		return this.result;
+	}
+}
+
+const calc = new Calculator();
+const res = calc.add(5).multiply(3).subtract(0).compute();
+
+console.log(res);
