565bfa5f9f8c02f4937e04da510b675903d42333
Migle Urbonaite <mig.urbonaite@gmail.com>
2022-10-05 14:02:07 +0200

    added command calc Commandc list and description

--- CommandCalc.js
+++ CommandCalc.js
@@ -1,3 +1,31 @@
+
+function add(x, y) { return x + y; }
+function sub(x, y) { return x - y; }
+function mul(x, y) { return x * y; }
+function div(x, y) { return x / y; }
+
+var Command = function (execute, undo, value) {
+    this.execute = execute;
+    this.undo = undo;
+    this.value = value;
+}
+
+var AddCommand = function (value) {
+    return new Command(add, sub, value);
+};
+
+var SubCommand = function (value) {
+    return new Command(sub, add, value);
+};
+
+var MulCommand = function (value) {
+    return new Command(mul, div, value);
+};
+
+var DivCommand = function (value) {
+    return new Command(div, mul, value);
+};
+
 var Calculator = function () {
     var current = 0;
     var commands = [];
@@ -11,13 +39,13 @@ var Calculator = function () {
         execute: function (command) {
             current = command.execute(current, command.value);
             commands.push(command);
-            console.log(action(command) + ": " + command.value);
+            console.log(action(command) + " : " + command.value);
         },
 
         undo: function () {
             var command = commands.pop();
             current = command.undo(current, command.value);
-            console.log("Undo " + action(command) + ": " + command.value);
+            console.log("Undo " + action(command) + " : " + command.value);
         },
 
         getCurrentValue: function () {
